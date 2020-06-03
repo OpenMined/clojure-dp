9275d1f7e73793f20616d966f65fcb8de4b5b91f
Lukasz Kozuchowski <lukasz.kozuchowski@gmail.com>
2020-06-03 21:46:46 +0200

    Add Apache License

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject differential-privacy-clj "0.1.1-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://github.com/OpenMined/differential-privacy-clj"
-  :license {:name "TODO"
-            :url "TODO"}
+  :license {:name "Apache License"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.google.protobuf/protobuf-java "3.11.4"]
                  [org.apache.commons/commons-math3 "3.6.1"]
